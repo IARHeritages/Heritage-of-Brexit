23a7d9aee1f4a8304624e45b935407aae9c5fc01
Chiara Bonacchi <chiara.bonacchi@gmail.com>
2016-10-17 09:58:38 +0100

    Facebook scraper
    Searches and scrapes public Facebook pages containing certain keywords in their titles

---
+++ Facebook-scraper.R
@@ -0,0 +1,33 @@
+## *Scraper for public Facebook pages that contain a specific keyword in their title*
+###Author: Chiara Bonacchi
+###Project: IARH
+
+# Set working directory. 
+setwd("~path")
+
+# Require Rfacebook, devtools and xlsx packages. If you have not installed htenm already, run install.packages() first.
+require(Rfacebook)
+require(devtools)
+require(xlsx)
+
+# Obtain a temporary user access token to access Facebook API from https://developers.facebook.com/tools-and-support/.
+token <- "EAACEdEose0cBAHQT4u564uNiMY3FvhkeyXddpOvpOOVhbuHK9v96ykpdTheB0DaJiQ0tyDaPBOJ3RWcC82oZBjGep1KXNgpXDPvZC6hqFMnOCFbFMITwlmleNEkfAZChjs3gFe4wBgnOCGFGga6K8w6mnF4uYtuZAyoBchiWHQZDZD"
+
+# Extract all the public Facebook pages (and reltaed information available) that contain the work "Medieval in their title".
+Medieval <- searchPages(string="Medieval", token=token, n=1000)
+
+# Save the list of public Facebook pages containing the world "Medieval" in an excel file.
+write.xlsx(Medieval, "medieval_Facebook-pages.xlsx")
+
+# Manually: (1) go through the list and remove entries that relate to public persons as these cannot be grabbed. (2) remove entries that relate to public Facebook pages that are not actually relevant to Medieval pasts. (3) integrate missing usernames.
+
+# Load the amended excel file.
+Medieval <- read.xlsx("medieval_Facebook-pages.xlsx", sheet=1)
+
+# Extract each of the entries in the list and extract their content, with a max of 10,000 post per page and save the content of each of them separately.
+usernames <- (medieval$username)
+for i in usernames {
+	i <- getPage(page=i, token=token, n=1000, feed=TRUE)
+	write.xlsx(i, "i.xlsx") 
+}
+
