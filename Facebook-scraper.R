c36f2c7a9f815e21e8ab2abf62d79fb4d3e30b19
Chiara Bonacchi <chiara.bonacchi@gmail.com>
2017-03-16 12:14:41 +0000

    Fixes to scraper

--- Facebook-scraper.R
+++ Facebook-scraper.R
@@ -19,15 +19,10 @@ Medieval <- searchPages(string="Medieval", token=token, n=1000)
 # Save the list of public Facebook pages containing the world "Medieval" in an excel file.
 write.xlsx(Medieval, "medieval_Facebook-pages.xlsx")
 
-# Manually: (1) go through the list and remove entries that relate to public persons as these cannot be grabbed. (2) remove entries that relate to public Facebook pages that are not actually relevant to Medieval pasts. (3) integrate missing usernames.
-
-# Load the amended excel file.
-Medieval <- read.xlsx("medieval_Facebook-pages.xlsx", sheet=1)
-
 # Extract each of the entries in the list and extract their content, with a max of 10,000 post per page and save the content of each of them separately.
 usernames <- (medieval$username)
 for i in usernames {
-	i <- getPage(page="i", token=token, n=1000, feed=TRUE)
-	write.xlsx(i, "i.xlsx") 
+	a <- getPage(page=i, token=token, n=1000, feed=TRUE)
+	write.xlsx(a, paste(i,".xlsx",sep="")) 
 }
 
